75a66880bd06172d3cc85f45fff9e661e75da86c
priyankasls-art <priyankasls@oipinsurtech.com>
2026-03-12 17:53:01 +0530

    Task 1.3: Code Style & Linting

---
+++ bad_style.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+
+print(add(2, 3))
+print(multiply(4, 5))
---
+++ test.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    return a + b
+
+
+x = add(5, 10)
+print(x)
